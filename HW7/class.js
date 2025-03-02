74adfed88e513000bd47f32d114dd60e79f4402c
Nikayla Schermer <Schernl3070@gmail.com>
2025-03-02 20:39:56 +0100

    Added photos

--- HW7/class.js
+++ HW7/class.js
@@ -10,32 +10,39 @@ class ViewMaster {
 
 const viewMasterCollection = [
     new ViewMaster(
-        "title 1",
-        "imgs/blue.png",
-        "description",
-        "author",
-        2000
+        "Migrant mother",
+        "imgs/Lange-MigrantMother.jpg",
+        "Taken during the Great Depression, this photograph of Florence Owens Thompson, a migrant mother, shows the struggles of poverty, oppression, and survival. It became an enduring symbol of the human cost of economic injustice and poverty in America.",
+        "Dorothea Lange",
+        1936
     ),
     new ViewMaster(
-        "title 2",
-        "imgs/darkBlue.png",
-        "description",
-        "author",
-        1950
+        "I am a man",
+        "imgs/IAmAMan.jpg",
+        "In 1787 English potter Josiah Wedgewood created an anti-slavery medallion which bore the phrase “Am I not a man and a brother?” In March 1968, striking Black sanitation workers in Memphis inverted that phrase, declaring instead of asking: “I am a Man.” Their version became a modern rallying cry for equal treatment.",
+        "Ernest Withers",
+        1968
     ),
     new ViewMaster(
-        "title 3",
-        "imgs/green.png",
-        "description",
-        "author",
-        1800
+        "Salt march",
+        "imgs/salt-march.jpg",
+        "The Salt march, also known as the Salt Act, Dandi March, and the Dandi march, was an act of nonviolent civil disobedience in colonial India, led by Mahatma Gandhi. The 24-day march lasted from 12 March 1930 to 6 April 1930 as a direct action campaign of tax resistance and nonviolent protest against the British salt monopoly",
+        "Unknown",
+        1930
     ),
     new ViewMaster(
-        "title 4",
-        "imgs/orange.png",
-        "description",
-        "author",
-        1900
+        "Nelson Mandela, free",
+        "imgs/nelson-mandela-free.jpg",
+        "This image captures the moment when Nelson Mandela was released from Robben Island prison in 1990 after 27 years of incarceration under the apartheid regime. His release symbolized the global victory over apartheid and the beginning of racial justice in South Africa.",
+        "Allan Tannenbaum",
+        1990
+    ),
+    new ViewMaster(
+        "Protests in Hong Kong",
+        "imgs/umbrella.jpg",
+        "A large group of protesters is marching through the streets of Hong Kong, each person holding an umbrella, with a sea of yellow and other colors. The umbrellas become a collective sign of solidarity in the fight against Beijing's influence over Hong Kong's political system. This image represents the democratic aspirations of the Hong Kong people.",
+        "Kin Cheung/AP",
+        2014
     )
 ];
 let previousIndex = -1;
